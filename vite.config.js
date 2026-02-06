71b906623859a12bb2827d0a4fecf61acbd06666
Ummadi Ajay <95040861+ummadi-ajay@users.noreply.github.com>
2026-02-06 17:58:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/ledger/',   // 👈 ADD THIS LINE
+
   plugins: [
     react(),
     VitePWA({
