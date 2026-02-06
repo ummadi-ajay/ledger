3bbce2c63ad0475f327361d1636a8bd171f5524b
Ummadi Ajay <ummadiaj@gmail.com>
2026-02-06 17:43:20 +0530

    Updated some changes

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBVgqwFQu3AlSqc7Khmwg05bUdKIDd8WrI",
-    authDomain: "ledgerapp-4f04a.firebaseapp.com",
-    projectId: "ledgerapp-4f04a",
-    storageBucket: "ledgerapp-4f04a.firebasestorage.app",
-    messagingSenderId: "227637983990",
-    appId: "1:227637983990:web:3e28c3f97d795740594ad7",
-    measurementId: "G-4T9V56HLY1"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
